3924d7d3a101269095d53b04d84c3a1d46361ef2
JohnZBurnett <tuninator@gmail.com>
2018-05-15 18:02:33 -0400

    monkey patch string

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences.reject! do |sentence|
+      sentence.empty?
+    end
+    sentences.length
   end
 end
+
+test_str = "This is a sentence. This is an emphatic sentence!! This is a querulous sentence?"
+test_str.count_sentences
